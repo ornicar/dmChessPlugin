3b8513bde4b6ab4610d5267f32c8f517e661fb0b
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-19 22:22:39 +0200

    Make broom a bit more aggressive

--- lib/model/doctrine/PluginDmChessGameTable.class.php
+++ lib/model/doctrine/PluginDmChessGameTable.class.php
@@ -6,13 +6,13 @@ class PluginDmChessGameTable extends myDoctrineTable
 
   public function broom()
   {
-    $query = $this->createQuery('g')
-    // games created yesterday, without a move
-    ->where('g.created_at < ? AND g.turns = ?', array(date ("Y-m-d H:i:s", strtotime('-1 day')), 0))
+    $this->createQuery('g')
+    // games created yesterday, with less than 3 moves
+    ->where('g.created_at < ? AND g.turns < ?', array(date ("Y-m-d H:i:s", strtotime('-1 day')), 3))
     // games with last move one month ago, and not finished
-    ->orWhere('g.updated_at < ? AND g.is_finished = ?', array(date ("Y-m-d H:i:s", strtotime('-1 month')), false));
-
-    $query->delete()->execute();
+    ->orWhere('g.updated_at < ? AND g.is_finished = ?', array(date ("Y-m-d H:i:s", strtotime('-1 month')), false))
+    // hop!
+    ->delete()->execute();
   }
 
   public function preload($id)
